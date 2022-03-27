8e6397566ab8fe15661c1217731c86a88aee4cfd
Cesar Torres <torres-cesar@outlook.com>
2022-03-27 00:05:20 -0500

    Add drag and drop function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import generateWebpage from './generate-webpage.js';
 import './style.css'
 
 async function main() {
+    /*
     async function everythingElse() {
         const ShipFactory = (shipLength) => {
             let tilesStatus = [];
@@ -62,13 +63,41 @@ async function main() {
         let carrier = await ShipFactory(5);
         console.log(carrier);
         return;
-    }
+    } 
+    */
     await generateWebpage();
-    await everythingElse();
+    let tugboat = await document.querySelector('.tugboat');
+    tugboat.addEventListener('mousedown', addDragandDrop);
+    // await everythingElse(); 
     return;
 }
 main();
 
+function addDragandDrop(event) {
+    let tugboat = document.querySelector('.tugboat');
+    tugboat.style.positon = 'absolute';
+    tugboat.style.zIndex = 1000;
+
+    document.body.append(tugboat);
+
+    function moveAt(pageX, pageY) {
+        tugboat.style.left = pageX - tugboat.offsetWidth / 2 + 'px';
+        tugboat.style.top = pageY - tugboat.offsetHeight / 2 + 'px';
+    }
+
+    moveAt(event.pageX, event.pageY);
+
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+      }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    tugboat.onmouseup = function() {
+        document.removeEventListener('mousemove', onMouseMove);
+        tugboat.onmouseup = null;
+    };
+}
 // generate gameboard
 // place ships on board
 // when user presses ready
