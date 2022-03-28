51a507b4998c065566e33dd2bce0781e5d919a96
Cesar Torres <torres-cesar@outlook.com>
2022-03-28 17:14:55 -0500

    Add algorithm handling ship placement

--- src/generate-webpage.js
+++ src/generate-webpage.js
@@ -5,7 +5,7 @@ export default async function generateWebpage() {
             for(let i = 1; i < 101; i++) {
                 let gridSquare = document.createElement('div');
                 gridSquare.setAttribute('id', i);
-                gridSquare.setAttribute('class', 'not-hit');
+                gridSquare.setAttribute('class', 'empty-grid');
                 gameboard.appendChild(gridSquare);
             }
             return;
@@ -15,73 +15,74 @@ export default async function generateWebpage() {
     }
     async function generatePlayerShips() {
         let container = document.querySelector('.container');
-        function dragAndDropShip(event, ship) {
+        function grabShip(event, ship) {
 
             ship.ondragstart = () => false;
 
             let shiftX = event.clientX - ship.getBoundingClientRect().left;
             let shiftY = event.clientY - ship.getBoundingClientRect().top;
 
-            // centers the ship at (pageX, pageY) coordinates
-            function repositionShipAt(pageX, pageY) {
+            function moveAt(pageX, pageY) {
                 ship.style.left = pageX - shiftX + 'px';
                 ship.style.top = pageY - shiftY + 'px';
             }
-            function onMouseDown() {
-                // make ship absolute and on top by z-index
-                ship.style.position = 'absolute';
-                ship.style.zIndex = 1000;
+            
+            // make ship absolute and on top by z-index
+            ship.style.position = 'absolute';
+            ship.style.zIndex = 1000;
 
-                // move it out of any current parents directly into body
-                // to make it positioned relative to the body
-                document.body.append(ship);
+            // move ship into body
+            document.body.append(ship);
 
-                // move absolutely positioned ship under the pointer
-                repositionShipAt(event.pageX, event.pageY);
-            }
-            function onMouseMove(event) {
+            // move ship under the pointer
+            moveAt(event.pageX, event.pageY);
+
+            function moveShip(event) {
                 function checkForDroppable() {
                     ship.hidden = true;
                     let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
                     ship.hidden = false;
+                    console.log(elemBelow);
 
                     if (!elemBelow) return;
 
-                    let droppableBelow = elemBelow.closest('.droppable');
+                    let emptyGrid = elemBelow.closest('.empty-grid');
+                    console.log(emptyGrid)
 
-                    if (currentDroppable != droppableBelow) {
-                        if(currentDroppable) { // already above a droppable
-                            // fly out of droppable
-                            leaveDroppable(currentDroppable);
+                    if (currentGrid != emptyGrid) {
+                        if(currentGrid) {
+                            function leaveGrid(grid) {
+                                grid.style.backgroundColor = "";
+                            }
+                            leaveGrid(currentGrid);
                         }
-                        currentDroppable = droppableBelow;
-                        if (currentDroppable) {
-                            // fly into droppable
-                            enterDroppable(currentDroppable);
+                        currentGrid = emptyGrid;
+                        if (currentGrid) {
+                            function enterGrid(grid) {
+                                grid.style.backgroundColor = "red";
+                            }
+                            enterGrid(currentGrid);
                         }
                     }
                 }
-                repositionShipAt(event.pageX, event.pageY);
+                moveAt(event.pageX, event.pageY);
                 checkForDroppable();
-            };
+            }
 
-            // (1) prepare ship on mousedown
-            onMouseDown();
-            
             // (2) move the ship on mousemove
-            let currentDroppable = null;
-            document.addEventListener('mousemove', onMouseMove);
+            let currentGrid = null;
+            document.addEventListener('mousemove', moveShip);
             
             // (3) drop the ship on mouseup, remove unneeded handlers
             ship.onmouseup = () => {
-                document.removeEventListener('mousemove', onMouseMove);
+                document.removeEventListener('mousemove', moveShip);
                 ship.onmouseup = null;
             };
         }
         function generateTugBoat() {
             let tugBoat = document.createElement('div');
             tugBoat.setAttribute('class', 'tugboat');
-            tugBoat.onmousedown = (event) => dragAndDropShip(event, tugBoat);
+            tugBoat.onmousedown = (event) => grabShip(event, tugBoat);
             container.appendChild(tugBoat);
         }
         generateTugBoat();
