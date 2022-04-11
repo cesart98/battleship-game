6c2c1eaefb30c05bb09f4ed282668e2d4dbf1d22
Cesar Torres <torres-cesar@outlook.com>
2022-04-10 19:20:24 -0500

    Reformat IIFE into class

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,51 +1,66 @@
-export const Gameboard = (() => {
-    let gameBoard = [];
-    for(let i = 1; i <= 100; i++) {
-        gameBoard[i] = {
-            isHit: false, 
-            hasShip: false, 
-            shipName: null
+class Gameboard {
+    constructor() {
+        let gridSquares = [];
+        for(let i = 1; i <= 100; i++) {
+            gridSquares[i] = {
+                isHit: false, 
+                hasShip: false, 
+                shipName: null
+            }
+        }
+        this.gridSquares = gridSquares;
+    }
+
+    receiveAttack(coordinate) {
+
+        let gridSquare = this.gridSquares[coordinate];
+
+        if(gridSquare.isHit == false) {
+            gridSquares[coordinate].isHit = true;
+        } if(gridSquare.hasShip == true) {
+            let ship = this.gridSquares[coordinate].shipName
+            ship.receiveAttack(coordinate);
         }
     }
 
-    const receiveAttackAt = (coordinate) => {
-        function checkIfHitAt(grid) {
-            if(grid.isHit == true) {
-                return;
-            } else if(grid.isHit == false) {
-                gameBoard[coordinate].isHit = true;
-                checkForShipAt(coordinate);
-                return;
-            };
-        }
-        function checkForShipAt(grid) {
-            if (grid.hasShip == false) {
-                return;
-            } else if(grid.hasShip == true) {
-                return; // send hit function to correct ship
-            }
-        }
-        return checkIfHitAt(gameBoard[coordinate]);
-    }
-    const placeShipAt = (ship, desiredGrids) => {
-        desiredGrids.forEach(grid => {
-            if (gameBoard[grid].hasShip == true) {
-                return; // reprompt for new location
-            } else if(gameBoard[grid].hasShip == false) {
-                gameBoard[grid].hasShip = true;
+    placeShipAt(ship, grids) {
+
+        grids.forEach((grid, index) => {
+
+            let coordinate  = grid[index];
+            let gridSquare = this.gridSquares[coordinate];
+
+            if(gridSquare.hasShip == false) {
+                this.gridSquares[coordinate].hasShip = true;
+                this.gridSquares[coordinate].shipName = ship.name;
             }
         })
-        ship.setLocation(desiredGrids);
     }
-    const getMissedAttacks = () => {
+
+    getMissedAttacks() {
         let missedAttacks = [];
-        gameBoard.forEach((grid, index) => {
-            if (grid.isHit == true && grid.hasShip == false) {
+        this.gridSquares.forEach((gridSquare, index) => {
+
+            if (gridSquare.isHit == true && gridSquare.hasShip == false) {
                 missedAttacks.push(index);
             }
         })
+
         return missedAttacks;
     }
-    const areShipsSunk = () => {};
-    return {receiveAttackAt, placeShipAt, getMissedAttacks, areShipsSunk};
-})();
+
+    areShipsSunk() {
+
+        let gridSquaresWithShips = this.gridSquares.filter(gridSquare => {
+            gridSquare.hasShip == true;
+        })
+
+        gridSquaresWithShips.every(gridSquare => {
+            gridSquare.isHit == true;
+        })
+        
+    }
+    
+}
+
+export {Gameboard};
