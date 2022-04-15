7f3282c3f02f7d1f0532b020f196c69a0988b8f0
Cesar Torres <torres-cesar@outlook.com>
2022-04-14 20:00:56 -0500

    Remove comments

--- src/ship-factory.js
+++ src/ship-factory.js
@@ -1,7 +1,7 @@
 export default class Ship {
     constructor(name, length) {
         this.shipParts = (Array(length)).fill(
-            { isHit: false, location: 'none' }
+            { isHit: false, location: null }
         );
         this.name = name;
     }
@@ -15,59 +15,12 @@ export default class Ship {
     }
 
     setLocation(grids) {
-
         let reformattedArray = this.shipParts.map((currentObj, index) => {
             let currentLocation = { location: grids[index] };
             return Object.assign( {}, currentObj, currentLocation );
         });
 
         this.shipParts = reformattedArray;
-
-        /*
-        this.shipParts = (Array.from(
-            (this.shipParts), (element, index) => {
-                Object.assign(element, { location: grids[index] });
-            }
-        ));
-        
-        this.shipParts.forEach((shipPart, index) => {
-            Object.assign(shipPart, { location: grids[index] });
-        })
-
-        
-        grids.forEach(function(grid, index) {
-            this.shipParts[index].location = grid;
-        }, this)
-        
-        this.shipParts[0] = grids[0];
-        this.shipParts[1] = grids[1];
-        this.shipParts[2] = grids[2];
-        this.shipParts[3] = grids[3];
-        this.shipParts[4] = grids[4];
-        
-        let gridsCopy = [...grids]
-        for(let i = 0; i <= gridsCopy.length; i++) {
-            Object.assign(this.shipParts, { location: gridsCopy[i]})
-        }
-
-        grids.forEach(function(grid, index)  {
-            console.log(this);
-            console.log(grid);
-            console.log(index)
-            let coordinate  = grid[index];
-
-            this.shipParts[index].location = grid;
-        }, this)
-    
-        
-        console.log(this);
-        this.shipParts.forEach((shipPart, index) => {
-            console.log(shipPart)
-            console.log(grids[index]);
-            console.log(this);
-            Object.assign(this.location, grids[index]);
-        })
-        */
     }
 
     isSunk() {
