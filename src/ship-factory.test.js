635c1d24e4a76100078fea3bddb421691990f409
Cesar Torres <torres-cesar@outlook.com>
2022-04-14 20:00:33 -0500

    Rename test descriptions

--- src/ship-factory.test.js
+++ src/ship-factory.test.js
@@ -1,21 +1,22 @@
 import Ship from './ship-factory';
 
-describe('intialize new carrier ship', () => {
+describe('correctly intialize new carrier ship', () => {
+
     let carrier = new Ship('carrier', 5);
 
     test('carrier is object', () => {
         expect(typeof(carrier)).toBe('object');
     });
 
-    test('carrier length is five', () => {
+    test('shipParts array length is five', () => {
         expect(carrier.shipParts.length).toEqual(5);
     });
 
-    test('carrier ship parts have correct start values', () => {
+    test('shipParts have correct start values', () => {
         carrier.shipParts.every(shipPart => {
             expect(shipPart.isHit).toBeFalsy();
             expect(shipPart.location).toBeNull();
-        })
+        });
     });
 
     test('carrier is not sunk', () => {
@@ -24,24 +25,27 @@ describe('intialize new carrier ship', () => {
     });
 });
 
-describe('test ship functions', () => {
+describe('correctly execute ship functions', () => {
+
     let carrier = new Ship('carrier', 5);
     let shipLocation = [21, 22, 23, 24, 25];
     let gridCoordinate = 24;
-    carrier.setLocation(shipLocation);
-    carrier.recieveAttack(gridCoordinate);
 
-    test('set ship location', () => {
-        carrier.shipParts.forEach((shipPart, index) => {
+    carrier.setLocation(shipLocation);
+
+    test('each shipPart location equals each value in array', () => {
+        carrier.shipParts.every((shipPart, index) => {
             expect(shipPart.location).toEqual(shipLocation[index]);
-        })
+        });
     });
 
-    test('attack ship part', () => {
+    carrier.recieveAttack(gridCoordinate);
+
+    test('shipPart that recieved attack is marked as hit', () => {
         carrier.shipParts.forEach((shipPart) => {
             if(shipPart.location === gridCoordinate) {
                 expect(shipPart.isHit).toBeTruthy();
-            }
-        })
+            };
+        });
     });
 });
