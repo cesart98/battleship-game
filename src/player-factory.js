ec114cbc9a797dc04ac60744079c16eda02441ef
Cesar Torres <torres-cesar@outlook.com>
2022-04-30 14:43:13 -0500

    Add functions to set gameboards

--- src/player-factory.js
+++ src/player-factory.js
@@ -1,6 +1,19 @@
 export default class Player {
     constructor(name) {
         this.name = name;
+        this.playerShips = {};
+        this.enemyGameboard = {};
+        this.playerGameboard = {};
+    }
+
+    setPlayerShips(shipsToAssign) {
+        this.playerShips = shipsToAssign;
+    }
+    setEnemyGameboard(gameboardToAssign) {
+        this.enemyGameboard = gameboardToAssign;
+    }
+    setPlayerGameboard(gameboardToAssign) {
+        this.playerGameboard = gameboardToAssign;
     }
 
     attackGameboard(gameboard, grid) {
