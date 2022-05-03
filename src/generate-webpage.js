a3bcfa3ff2c4717db34a5c288a3d4dc31b6b4219
Cesar Torres <torres-cesar@outlook.com>
2022-05-03 03:27:20 -0500

    Remove function to add div elements

--- src/generate-webpage.js
+++ src/generate-webpage.js
@@ -1,19 +1,6 @@
 import grabElement from './grab-element'
 
 export default async function generateWebpage() {
-    async function generateGridSquares() {
-        let gameboard = document.querySelector('.gameboard');
-
-        for(let i = 0; i < 100; i++) {
-            let gridSquare = document.createElement('div');
-            gridSquare.setAttribute('id', i);
-            gridSquare.classList.add('droppable');
-            gridSquare.classList.add('empty');
-            gameboard.appendChild(gridSquare);
-        }
-
-        return;
-    }
     async function addDragDropBehavior() {
         let shipContainer = document.querySelector('.ship-container');
         let shipElements = Array.from(shipContainer.childNodes).filter(
@@ -27,7 +14,6 @@ export default async function generateWebpage() {
 
         return;
     }
-    await generateGridSquares();
     await addDragDropBehavior();
 
     return;
