1317c59fae5996ee2807121f82a50b505df9a9f2
Cesar Torres <torres-cesar@outlook.com>
2022-05-06 22:49:08 -0500

    Add shift in element positioning

--- src/grab-element.js
+++ src/grab-element.js
@@ -1,34 +1,49 @@
 export default function addDragDropBehavior(mousedownEvent, elem) {
     const mouse = (() => {
-        function moveElem(pageX, pageY) {
-            elem.style.left = pageX - elem.offsetWidth / 2 + 'px';
-            elem.style.top = pageY - elem.offsetHeight / 2 + 'px';
+        const moveElem = (pageX, pageY) => {
+            elem.style.left = pageX - shiftX + 'px';
+            elem.style.top = pageY - shiftY + 'px';
         }
         const grabElem = (event) => {
             elem.style.position = 'absolute';
             elem.style.zIndex = 1000;
-            elem.style.width = (mousedownEvent.path[2].scrollWidth) / 10 + 'px';
+            elem.style.width = originalContainer.scrollWidth / 10 + 'px';
             document.body.append(elem);
-    
-            moveElem(event.pageX, event.pageY);
-        }
-        const dragElem = (event) => {
-            elem.ondragstart = () => false;
-            
-            moveElem(event.pageX, event.pageY);
+
+            mouse.moveElem(event.pageX, event.pageY);
         }
         const dropElem = () => {
             elem.style.position = 'static';
             elem.style.zIndex = 'auto';
+            elem.style.width = 'auto;'
         }
-        return {grabElem, dragElem, dropElem}
+        const rejoinElem = (event) => {
+            shiftX = event.clientX - elemChildren[0].getBoundingClientRect().left;
+            shiftY = event.clientY - elemChildren[0].getBoundingClientRect().top;
+            mouse.grabElem(event);
+            elemChildren.forEach(elemChild => {
+                elemChild.parentNode.classList.remove('occupied');
+                elemChild.parentNode.classList.add('empty');
+                elem.appendChild(elemChild);
+            });
+        }
+        return { moveElem, grabElem, dropElem, rejoinElem };
     })();
     const elementsBelow = (() => {
-        let elemChildren = Array.from(elem.children);
         let currentElementsBelow = [];
-        let previousElementsBelow;
-        
-        const setCurrentElementsBelow = () => {
+        let previousElementsBelow = null;
+
+        function addClassNames(elementsArray, className) {
+            elementsArray.forEach(element => {
+                element.classList.add(className);
+            })
+        }
+        function removeClassNames(elementsArray, className) {
+            elementsArray.forEach(element => {
+                element.classList.remove(className);
+            })
+        }
+        const set = () => {
             function getElemBelow(x, y) {
                 elemChildren.forEach(elemChild => {
                     elemChild.hidden = true;
@@ -42,102 +57,79 @@ export default function addDragDropBehavior(mousedownEvent, elem) {
                 return elemBelowMouse;
             }
             elemChildren.forEach((shipPart, index) => {
-                let shipPartX = (shipPart.getBoundingClientRect().left + (shipPart.getBoundingClientRect().width * 0.5));
-                let shipPartY = (shipPart.getBoundingClientRect().top + (shipPart.getBoundingClientRect().height * 0.5));
+                let shipPartX = shipPart.getBoundingClientRect().left + (shipPart.getBoundingClientRect().width * 0.5);
+                let shipPartY = shipPart.getBoundingClientRect().top + (shipPart.getBoundingClientRect().height * 0.5);
                 let elemBelow = getElemBelow(shipPartX, shipPartY);
                 currentElementsBelow[index] = elemBelow;
             })
         }
-        const haveClassName = (className) => {
-            setCurrentElementsBelow();
-            if(currentElementsBelow.every(elementBelow => elementBelow.classList.contains(className))) {
-                return true;
-            }
-        }
-        const classifyAsEmpty = () => {
-            currentElementsBelow.forEach(elementBelow => {
-                elementBelow.classList.replace('occupied', 'empty');
-            })
-        }
-        const classifyAsOccupied = () => {
-            currentElementsBelow.forEach(elementBelow => {
-                elementBelow.classList.replace('empty', 'occupied');
-            })
+        const areAll = (className) => {
+            if(currentElementsBelow.some(element => !element.classList.contains(className))) {
+                return false;
+            } else  { return true; }
         }
         const handleHoverEffect = () => {
-            // if every element below is droppable...
-            if(currentElementsBelow.every(elementBelow => elementBelow.classList.contains('droppable'))) {
-                
-                // add hover class name
-                currentElementsBelow.forEach(elementBelow => {
-                    elementBelow.classList.add('hover');
-                })
-
-                // set previous elements array if not set.
-                if(!previousElementsBelow) {
-                    previousElementsBelow = Array.from(currentElementsBelow);
+            if(currentElementsBelow != previousElementsBelow) {
+                if(previousElementsBelow) {
+                    removeClassNames(previousElementsBelow, 'hover');
                 }
-
-                // if any element does not equal the previous one...
-                if(currentElementsBelow.some((elementBelow, index) => elementBelow != previousElementsBelow[index])) {
-                    previousElementsBelow.forEach(elementBelow => {
-                        // ...remove hover class for previous element if not in current elements
-                        if(!currentElementsBelow.includes(elementBelow)) {
-                            elementBelow.classList.remove('hover')
-                        }
-                    })
-                    previousElementsBelow = Array.from(currentElementsBelow);
-                }
-            }
-            // if any element below is not droppable...
-            if(previousElementsBelow && currentElementsBelow.some(elementBelow => !elementBelow.classList.contains('droppable'))) {
-                previousElementsBelow.forEach(elementBelow => {
-                    elementBelow.classList.remove('hover');
-                })
-                previousElementsBelow = null;
+                previousElementsBelow = Array.from(currentElementsBelow);
+                addClassNames(previousElementsBelow, 'hover');
             }
         }
         const receiveElement = () => {
+            removeClassNames(currentElementsBelow, 'hover');
+            removeClassNames(currentElementsBelow, 'empty');
             currentElementsBelow.forEach((elementBelow, index) => {
                 elementBelow.appendChild(elemChildren[index]);
             })
-            elemChildren.forEach(elemChild => { // add event listener to remove from board
-                elemChild.addEventListener('mousedown', (event) => {
-                    elemChildren.forEach(elemChild => elem.appendChild(elemChild));
-                    mouse.grabElem(event);
-                    elementsBelow.classifyAsEmpty();
-                    addDragDropBehavior(event, elem);
-                })
-            })
+            addClassNames(currentElementsBelow, 'occupied');
         }
-        return {setCurrentElementsBelow, haveClassName, classifyAsEmpty, classifyAsOccupied, handleHoverEffect, receiveElement}
+        return {set, areAll, handleHoverEffect, receiveElement}
     })();
-    console.log({elem})
-    console.log(mousedownEvent)
+
+    let originalContainer = document.querySelector('.ship-container');
+    let elemChildren = Array.from(elem.children);
+    let shiftX = mousedownEvent.clientX - elem.getBoundingClientRect().left;
+    let shiftY = mousedownEvent.clientY - elem.getBoundingClientRect().top;
+    elem.ondragstart = () => false;
+
     // (1) append elem under the mouse
     mouse.grabElem(mousedownEvent);
 
-    if(elementsBelow.haveClassName('occupied')) {
-        elementsBelow.classifyAsEmpty();
+    elementsBelow.set();
+    if(elementsBelow.areAll('droppable')) {
+        elementsBelow.handleHoverEffect();
     }
 
     // (2) move elem as mouse moves
     document.onmousemove = (mousemoveEvent) => {
-        mouse.dragElem(mousemoveEvent);
+        mouse.moveElem(mousemoveEvent.pageX, mousemoveEvent.pageY);
 
-        elementsBelow.setCurrentElementsBelow();
-        elementsBelow.handleHoverEffect();
+        elementsBelow.set();
+        if(elementsBelow.areAll('droppable')) {
+            elementsBelow.handleHoverEffect();
+        }
     }
     
     // (3) drop elem on mouseup,
-    elem.onmouseup = (mouseupEvent) => {
+    elem.onmouseup = () => {
         document.onmousemove = null;
         mouse.onmouseup = null;
 
-        if(elementsBelow.haveClassName('empty')) {
-            mouse.dropElem(mouseupEvent);
+        if(elementsBelow.areAll('empty')) {
+            mouse.dropElem();
             elementsBelow.receiveElement();
-            elementsBelow.classifyAsOccupied();
+
+            // add event listener for next event
+            elemChildren.forEach(elemChild => {
+                elemChild.addEventListener('mousedown', event => {
+                    mouse.rejoinElem(event);
+                    addDragDropBehavior(event, elem);
+                });
+            })
         }
+
+        
     }
 }
